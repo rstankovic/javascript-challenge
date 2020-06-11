a03e25f5274288f9e8e33dc6733b4f17ce9eadb5
rstankovic <cschristianstankovic@gmail.com>
2020-06-11 14:58:33 -0500

    finished part one, not sure about part two not really feeling it. i dont really like java

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,38 @@
 // from data.js
-var tableData = data;
+const tableData = [...data];
 
 // YOUR CODE HERE!
+function createTable(newdata) {
+    var mainTable = d3.select('tbody');
+    newdata.forEach(
+        function(sighting) {
+            var row = mainTable.append('tr');
+            row.append('td').text(`${sighting.datetime}`);
+            row.append('td').text(`${sighting.city}`);
+            row.append('td').text(`${sighting.state}`);
+            row.append('td').text(`${sighting.country}`);
+            row.append('td').text(`${sighting.shape}`);
+            row.append('td').text(`${sighting.durationMinutes}`);
+            row.append('td').text(`${sighting.comments}`);
+        }
+    );
+}
+function updatePage() {
+    console.log("updateTable has been called");
+    var filterByDate = document.getElementById("datetime").value;
+    var filteredData = [...tableData];
+    filteredData.forEach(
+        function(sighting, index) {
+            if (sighting.datetime !== filterByDate) {
+                delete filteredData[index];
+            } else {
+            }
+        });
+        console.log(filteredData);
+    var trs = d3.select('tbody').selectAll("tr");
+    trs.remove();
+    createTable(filteredData);
+}
+createTable(tableData);
+// var filterBtn = d3.select('#filter-btn');
+// filterBtn.on("click", updatePage());
